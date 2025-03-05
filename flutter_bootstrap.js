e0aaf4b3532ec837baaac0559c31cef6d271f11d
iritep <10761851+iritep@users.noreply.github.com>
2025-03-05 08:04:47 -0600

    feat: all pages are visible - refactor finished

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2960002351"
+    serviceWorkerVersion: "813157962"
   }
 });
