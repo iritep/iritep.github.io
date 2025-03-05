c5d04f062874f5c53be15f599bae54e218a02577
iritep <10761851+iritep@users.noreply.github.com>
2025-03-05 01:24:10 -0600

    fix: corrected hidden case in main component

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4185087861"
+    serviceWorkerVersion: "1216586912"
   }
 });
