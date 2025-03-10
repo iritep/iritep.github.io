c96483f98abb4bdabd77ff271c18dfcfedeff89c
iritep <10761851+iritep@users.noreply.github.com>
2025-03-10 08:44:13 -0500

    fix: completed responsiveness on screens for mobile breakpoints

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4064568632"
+    serviceWorkerVersion: "2721373239"
   }
 });
